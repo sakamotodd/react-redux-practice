0be657c2839784e289361f801b6d7b0bb086b154
坂本大五郎 <skmtdgr@daigoro.local>
2020-12-28 15:33:49 +0900

    implement component function

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+//ActionはjavaScriptのオブジェクト
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+  type: 'INCREMENT'
+});
+export const decrement = () => ({
+  type: 'DECREMENT'
+});
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import reducer from './reducers';
+// import registerServiceWorker from './registerServiceWorker';
 
-
+const store = createStore(reducer);
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
+
   document.getElementById('root')
 );
+// registerServiceWorker();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
