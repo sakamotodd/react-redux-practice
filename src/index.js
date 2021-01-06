94531685a2868f95c409a9c90a256c774dd8b0de
坂本大五郎 <skmtdgr@daigoro.local>
2021-01-07 07:26:30 +0900

    event-index axios redux-thunc

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import './index.css';
-import App from './components/App';
-import reducer from './reducers';
-// import registerServiceWorker from './registerServiceWorker';
+import thunk from 'redux-thunk'
 
-const store = createStore(reducer);
+import './index.css';
+import EventIndex from './components/events_index';
+import reducer from './reducers';
+
+const store = createStore(reducer, applyMiddleware(thunk));
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <EventIndex />
   </Provider>,
 
   document.getElementById('root')
 );
-// registerServiceWorker();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
