5ba77333038c23ad4e3aab009a5c810e491a31e7
sakamotodd <73228728+sakamotodd@users.noreply.github.com>
2021-02-17 07:39:22 +0900

    Link View

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import './index.css';
 import EventIndex from './components/event_index';
+import EventNew from './components/event_new';
 import reducer from './reducers';
 
 
 const store = createStore(reducer, applyMiddleware(thunk));
 ReactDOM.render(
   <Provider store={store}>
-    <EventIndex />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/events/new" component={EventNew}/>
+        <Route exact path="/" component={EventIndex}/>
+      </Switch>
+    </BrowserRouter>
   </Provider>,
 
   document.getElementById('root')
